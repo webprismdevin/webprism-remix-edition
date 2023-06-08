353b8d7978c5c9ff15d2f9ffab4d891ce3032a6e
Devin Santamaria <devin@webprism.co>
2023-06-08 14:53:25 -0700

    preview for ash

--- app/components/marquee.tsx
+++ app/components/marquee.tsx
@@ -3,77 +3,93 @@ import { urlFor } from "~/routes/_index";
 
 const random = () => Math.random().toString(36).substring(7);
 
-export function Marquee({ icon, items }: { icon: any, items: [string] }) {
-    return (
-        <div className="flex select-none gap-[3rem] overflow-hidden py-4">
-            <div className="scroll flex min-w-full flex-shrink-0 items-center justify-around gap-[3rem]">
-                {items.map((item: any) => (
-                    <Fragment key={random()}>
-                        <div>
-                            <span className="font-heading text-4xl uppercase">{item}</span>
-                        </div>
-                        <div>
-                            <img
-                                src={urlFor(icon).url()}
-                                width="36px"
-                                height="36px"
-                                alt="decorative icon"
-                                loading="lazy"
-                            />
-                        </div>
-                    </Fragment>
-                ))}
-                {items.map((item: any) => (
-                    <Fragment key={random()}>
-                        <div>
-                            <span className="font-heading text-4xl uppercase">{item}</span>
-                        </div>
-                        <div>
-                            <img
-                                src={urlFor(icon).url()}
-                                width="36px"
-                                height="36px"
-                                alt="decorative icon"
-                                loading="lazy"
-                            />
-                        </div>
-                    </Fragment>
-                ))}
+export function Marquee({
+  icon,
+  items,
+  reverse,
+}: {
+  icon: any;
+  items: [string];
+  reverse?: boolean;
+}) {
+  return (
+    <div className="flex select-none gap-[3rem] overflow-hidden py-4">
+      <div
+        className={`${
+          reverse && "reverse"
+        } scroll flex min-w-full flex-shrink-0 items-center justify-around gap-[3rem]`}
+      >
+        {items.map((item: any) => (
+          <Fragment key={random()}>
+            <div>
+              <span className="font-heading text-4xl uppercase">{item}</span>
             </div>
-            <div className="scroll flex min-w-full flex-shrink-0 items-center justify-around gap-[3rem]">
-                {items.map((item: any) => (
-                    <Fragment key={random()}>
-                        <div>
-                            <span className="font-heading text-4xl uppercase">{item}</span>
-                        </div>
-                        <div>
-                            <img
-                                src={urlFor(icon).url()}
-                                width="36px"
-                                height="36px"
-                                alt="decorative icon"
-                                loading="lazy"
-                            />
-                        </div>
-                    </Fragment>
-                ))}
-                {items.map((item: any) => (
-                    <Fragment key={random()}>
-                        <div>
-                            <span className="font-heading text-4xl uppercase">{item}</span>
-                        </div>
-                        <div>
-                            <img
-                                src={urlFor(icon).url()}
-                                width="36px"
-                                height="36px"
-                                alt="decorative icon"
-                                loading="lazy"
-                            />
-                        </div>
-                    </Fragment>
-                ))}
+            <div>
+              <img
+                src={urlFor(icon).url()}
+                width="36px"
+                height="36px"
+                alt="decorative icon"
+                loading="lazy"
+              />
             </div>
-        </div>
-    );
+          </Fragment>
+        ))}
+        {items.map((item: any) => (
+          <Fragment key={random()}>
+            <div>
+              <span className="font-heading text-4xl uppercase">{item}</span>
+            </div>
+            <div>
+              <img
+                src={urlFor(icon).url()}
+                width="36px"
+                height="36px"
+                alt="decorative icon"
+                loading="lazy"
+              />
+            </div>
+          </Fragment>
+        ))}
+      </div>
+      <div
+        className={`${
+          reverse && "reverse"
+        }  scroll flex min-w-full flex-shrink-0 items-center justify-around gap-[3rem]`}
+      >
+        {items.map((item: any) => (
+          <Fragment key={random()}>
+            <div>
+              <span className="font-heading text-4xl uppercase">{item}</span>
+            </div>
+            <div>
+              <img
+                src={urlFor(icon).url()}
+                width="36px"
+                height="36px"
+                alt="decorative icon"
+                loading="lazy"
+              />
+            </div>
+          </Fragment>
+        ))}
+        {items.map((item: any) => (
+          <Fragment key={random()}>
+            <div>
+              <span className="font-heading text-4xl uppercase">{item}</span>
+            </div>
+            <div>
+              <img
+                src={urlFor(icon).url()}
+                width="36px"
+                height="36px"
+                alt="decorative icon"
+                loading="lazy"
+              />
+            </div>
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/components/slides.tsx
+++ app/components/slides.tsx
@@ -1,167 +1,173 @@
-import { AnimatePresence, motion } from 'framer-motion';
-import { wrap } from '@popmotion/popcorn';
-import { useState } from 'react';
-import { urlFor } from '~/routes/_index'
-import { Link } from '@remix-run/react';
+import { AnimatePresence, motion } from "framer-motion";
+import { wrap } from "@popmotion/popcorn";
+import { useState } from "react";
+import { urlFor } from "~/routes/_index";
+import { Link } from "@remix-run/react";
 
 export interface Slide {
-    id: string;
-    image: any;
-    image2?: any;
-    title: string;
-    caption: string;
-    cta: {
-        text: string;
-        to: string;
-    };
+  id: string;
+  image: any;
+  image2?: any;
+  title: string;
+  caption: string;
+  cta: {
+    text: string;
+    to: string;
+  };
 }
 
 export default function Slides({ slides }: { slides: any }) {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const index = wrap(0, slides.length, page);
 
-    const [[page, direction], setPage] = useState([0, 0]);
-    const index = wrap(0, slides.length, page);
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
+  };
 
-    const paginate = (newDirection: number) => {
-        setPage([page + newDirection, newDirection]);
-    };
-
-    return (
+  return (
+    <motion.div
+      className="relative h-[500px] p-8 md:p-16"
+      custom={direction}
+      key={page}
+    >
+      <AnimatePresence mode="wait">
         <motion.div
-            className="relative h-[700px] border-b-4 border-white border-t-4 bg-black"
-            custom={direction}
-            key={page}
+          initial={{ x: 800 * direction, opacity: 0 }}
+          animate={{ x: 0, opacity: 1, transition: { duration: 0.5 } }}
+          exit={{
+            x: -800 * direction,
+            opacity: 0,
+            transition: { duration: 0.2 },
+          }}
+          custom={direction}
+          key={page}
+          className={`relative rounded overflow-hidden grid h-full w-full grid-cols-1 bg-white ${
+            slides[index].image2 ? "lg:grid-cols-2" : ""
+          }`}
         >
-            <AnimatePresence mode="wait">
-                <motion.div
-                    initial={{ x: -800 * direction, opacity: 0 }}
-                    animate={{ x: 0, opacity: 1, transition: { duration: 0.5 } }}
-                    exit={{ x: 800 * direction, opacity: 0, transition: { duration: 0.2 } }}
-                    custom={direction}
-                    key={page}
-                    className={`relative grid h-full w-full grid-cols-1 bg-white ${slides[index].image2 ? 'lg:grid-cols-2' : ''
-                        }`}
-                >
-                    <div className="absolute z-10 grid h-full w-full place-items-center px-12">
-                        <div className="text-center">
-                            <p className="text-shadow mb-4">{slides[index].subtitle}</p>
-                            <h2 className="text-shadow mb-6 max-w-xl text-center font-heading text-4xl uppercase md:text-5xl lg:text-6xl">
-                                {slides[index].title}
-                            </h2>
-                            {slides[index].cta?.to && <Link to={slides[index].cta?.to}>
-                                {slides[index].cta.text}
-                            </Link>}
-                        </div>
-                    </div>
-                    <div
-                        className={`h-full min-w-full max-w-full overflow-hidden ${slides[index].image2 ? 'lg:max-w-1/2' : ''
-                            }`}
-                    >
-                        {slides[index].image && <img
-                            src={urlFor(slides[index].image)
-                                .width(1200)
-                                .height(1200)
-                                .format('webp')
-                                .quality(80)
-                                .url()}
-                            sizes={slides[index].image2 ? '50vw' : '100vw'}
-                            className={`min-h-full min-w-full object-cover ${slides[index].image2 ? '' : 'lg:min-w-full'
-                                }`}
-                            alt={slides[index].image?.alt}
-                        />}
-                    </div>
-                    {slides[index].image2 && (
-                        <div className="max-w-1/2 hidden h-full overflow-hidden lg:block">
-                            <img
-                                src={urlFor(slides[index].image2)
-                                    .width(1200)
-                                    .height(1200)
-                                    .format('webp')
-                                    .quality(80)
-                                    .url()}
-                                sizes={'50vw'}
-                                height={slides[index].image2.height}
-                                width={slides[index].image2.width}
-                                className="min-h-full object-cover"
-                                alt={slides[index].image2?.alt}
-                            />
-                        </div>
-                    )}
-                </motion.div>
-            </AnimatePresence>
-            {/* back arrow */}
-            <NavArrowLeft
-                className="absolute left-5 top-1/2 z-10 -translate-y-1/2 transform cursor-pointer"
-                aria-label="previous"
-                onClick={() => paginate(-1)}
-            />
-            {/* next arrow */}
-            <NavArrowRight
-                className="absolute right-5 top-1/2 z-10 -translate-y-1/2 transform cursor-pointer"
-                aria-label="next"
-                onClick={() => paginate(1)}
-            />
+          <div className="absolute z-10 grid h-full w-full place-items-center px-12">
+            <div className="text-center">
+              <p className="text-shadow mb-4">{slides[index].subtitle}</p>
+              <h2 className="text-shadow mb-6 max-w-xl text-center font-heading text-4xl uppercase md:text-5xl lg:text-6xl">
+                {slides[index].title}
+              </h2>
+              {slides[index].cta?.to && (
+                <Link to={slides[index].cta?.to}>{slides[index].cta.text}</Link>
+              )}
+            </div>
+          </div>
+          <div
+            className={`h-full min-w-full max-w-full overflow-hidden ${
+              slides[index].image2 ? "lg:max-w-1/2" : ""
+            }`}
+          >
+            {slides[index].image && (
+              <img
+                src={urlFor(slides[index].image)
+                  .format("webp")
+                  .quality(80)
+                  .url()}
+                sizes={slides[index].image2 ? "50vw" : "100vw"}
+                className={`min-h-full min-w-full object-cover ${
+                  slides[index].image2 ? "" : "lg:min-w-full"
+                }`}
+                alt={slides[index].image?.alt}
+              />
+            )}
+          </div>
+          {slides[index].image2 && (
+            <div className="max-w-1/2 hidden h-full overflow-hidden lg:block">
+              <img
+                src={urlFor(slides[index].image2)
+                  .width(1200)
+                  .height(1200)
+                  .format("webp")
+                  .quality(80)
+                  .url()}
+                sizes={"50vw"}
+                height={slides[index].image2.height}
+                width={slides[index].image2.width}
+                className="min-h-full object-cover"
+                alt={slides[index].image2?.alt}
+              />
+            </div>
+          )}
         </motion.div>
-    );
+      </AnimatePresence>
+      {/* back arrow */}
+      <NavArrowLeft
+        className="absolute left-1 md:left-4 top-1/2 z-10 -translate-y-1/2 transform cursor-pointer"
+        aria-label="previous"
+        onClick={() => paginate(-1)}
+      />
+      {/* next arrow */}
+      <NavArrowRight
+        className="absolute right-1 md:right-4 top-1/2 z-10 -translate-y-1/2 transform cursor-pointer"
+        aria-label="next"
+        onClick={() => paginate(1)}
+      />
+    </motion.div>
+  );
 }
 
 export const NavArrowLeft = ({
-    color,
-    size,
-    ...props
+  color,
+  size,
+  ...props
 }: {
-    color?: string;
-    size?: number;
-    props: any;
+  color?: string;
+  size?: number;
+  props: any;
 }) => {
-    return (
-        <svg
-            {...props}
-            width={size ? size : 24}
-            height={size ? size : 24}
-            strokeWidth="1.5"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            color={color !== undefined ? color : '#ffffff'}
-        >
-            <path
-                d="M15 6l-6 6 6 6"
-                stroke={color !== undefined ? color : '#ffffff'}
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            ></path>
-        </svg>
-    );
+  return (
+    <svg
+      {...props}
+      width={size ? size : 24}
+      height={size ? size : 24}
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color={color !== undefined ? color : "#ffffff"}
+    >
+      <path
+        d="M15 6l-6 6 6 6"
+        stroke={color !== undefined ? color : "#ffffff"}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
 };
 
 export const NavArrowRight = ({
-    color,
-    size,
-    ...props
+  color,
+  size,
+  ...props
 }: {
-    color?: string;
-    size?: number;
-    props: any;
+  color?: string;
+  size?: number;
+  props: any;
 }) => {
-    return (
-        <svg
-            {...props}
-            width={size ? size : 24}
-            height={size ? size : 24}
-            strokeWidth="1.5"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            color={color !== undefined ? color : '#ffffff'}
-        >
-            <path
-                d="M9 6l6 6-6 6"
-                stroke={color !== undefined ? color : '#ffffff'}
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            ></path>
-        </svg>
-    );
+  return (
+    <svg
+      {...props}
+      width={size ? size : 24}
+      height={size ? size : 24}
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color={color !== undefined ? color : "#ffffff"}
+    >
+      <path
+        d="M9 6l6 6-6 6"
+        stroke={color !== undefined ? color : "#ffffff"}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  );
 };

16b5839feae7ff2595262dca101620a29eb0a96b
Devin Santamaria <devin@webprism.co>
2023-06-12 13:53:41 -0700

    seo updates

--- app/routes/$pageSlug.tsx
+++ app/routes/$pageSlug.tsx
@@ -1,6 +1,6 @@
 import { LoaderArgs, LoaderFunction } from "@remix-run/node";
-import { Modules, sanity } from "./_index";
-import { useLoaderData } from "@remix-run/react";
+import { Modules, sanity, urlFor } from "./_index";
+import { V2_MetaFunction, useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async ({
   request,
@@ -14,6 +14,21 @@ export const loader: LoaderFunction = async ({
   return {
     data,
   };
+};
+
+export const meta: V2_MetaFunction<typeof loader> = ({ data }) => {
+  const { data: loaderData } = data;
+
+  return [
+    { title: loaderData.seo?.title },
+    { name: "description", content: loaderData.seo?.description },
+    {
+      property: "og:image",
+      content: loaderData.seo?.image
+        ? urlFor(loaderData.seo?.image).width(1200).height(630).url()
+        : null,
+    },
+  ];
 };
 
 export default function Page() {

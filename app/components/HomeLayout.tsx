2a9d7246901b223cef3d9b305def367454d17130
Devin Santamaria <devin@webprism.co>
2023-09-16 22:58:36 -0700

    fix overflow, hide scrollbars

--- app/components/HomeLayout.tsx
+++ app/components/HomeLayout.tsx
@@ -4,7 +4,7 @@ export default function ({data}: any) {
   return (
     <>
       <div
-        className="grid place-content-center p-4 absolute top-0 left-0 right-0 w-screen z-50"
+        className="grid place-content-center p-4 absolute top-0 left-0 right-0 z-50"
       >
         <div className="font-heading uppercase text-cta text-white">webprism</div>
       </div>

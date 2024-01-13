a78e5ad353f1b4eacfb3d4e034adbea7f28d7ceb
Devin Santamaria <devin@webprism.co>
2024-01-13 14:37:37 -0800

    update menu to headlessui

--- app/root.tsx
+++ app/root.tsx
@@ -20,6 +20,7 @@ import { loadQuery } from "./sanity/loader.server";
 import groq from "groq";
 import { INTERNAL_LINK_FRAGMENT } from "./routes/_index";
 import { useQuery } from "./sanity/loader";
+import { Menu } from "@headlessui/react";
 
 const VisualEditing = lazy(() => import("~/components/VisualEditing"));
 
@@ -107,7 +108,8 @@ const Layout = ({ children, menu }: { children: ReactNode; menu: any[] }) => {
         <Link to={"/"} className="font-heading uppercase text-xl md:text-2xl">
           WEBPRISM
         </Link>
-        <div className="flex gap-2 md:gap-5">
+        <MenuDropdown menu={menu} />
+        {/* <div className="flex gap-2 md:gap-5">
           {menu?.map((item) => {
             if (item._type == "linkExternal") {
               return (
@@ -122,10 +124,66 @@ const Layout = ({ children, menu }: { children: ReactNode; menu: any[] }) => {
               </Link>
             );
           })}
-        </div>
+        </div>*/}
       </div>
       {children}
       {/* <div>Footer</div> */}
     </>
+  );
+};
+
+const MenuDropdown = ({ menu }: { menu: any[] }) => {
+  return (
+    <div className="relative inline-block text-left">
+      <Menu>
+        {({ open }) => (
+          <>
+            <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+              Menu
+            </Menu.Button>
+            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                {menu?.map((item) => {
+                  if (item._type == "linkExternal") {
+                    return (
+                      <Menu.Item key={item._key}>
+                        {({ active }) => (
+                          <a
+                            href={item.url}
+                            className={`${
+                              active
+                                ? "bg-gray-100 text-gray-900"
+                                : "text-gray-700"
+                            } flex justify-between w-full px-4 py-2 text-sm`}
+                          >
+                            {item.title}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    );
+                  }
+                  return (
+                    <Menu.Item key={item._key}>
+                      {({ active }) => (
+                        <Link
+                          to={item.to}
+                          className={`${
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700"
+                          } flex justify-between w-full px-4 py-2 text-sm`}
+                        >
+                          {item.title}
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  );
+                })}
+              </div>
+            </Menu.Items>
+          </>
+        )}
+      </Menu>
+    </div>
   );
 };

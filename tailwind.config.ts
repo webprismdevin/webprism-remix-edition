b33a5fd130ac772a35a5529146c7951cca46e11e
Devin Santamaria <devin@webprism.co>
2024-03-04 16:32:04 -0800

    readd shadcn-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,24 @@
 import type { Config } from "tailwindcss";
 const defaultTheme = require("tailwindcss/defaultTheme");
 
-export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./@/components/ui/*.{ts,tsx}"],
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./@/components//**/*.{ts,tsx}",
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         heading: ["BebasNeue-Regular", "ui-sans-serif", "system-ui"],
@@ -16,7 +31,23 @@ export default {
       minHeight: {
         hero: "calc(100vh - 64px)",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
+
+export default config;
